61b5f6e9f512fce805a095dc036e62d780f4bdd1
mzimperdido-ui <mzimperdido@gmail.com>
2026-01-03 00:45:52 -0300

    Upmain.py

--- c-realV2.py
+++ c-realV2.py
@@ -1,32 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-MIT License
-
-Copyright (c) 2020 - 2025
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-Don't use the bot on real servers or use it to spam because this is breaking
-discord's ToS, and you will be resulted in an account deletion.
-"""
-# discord
 import discord, sys, requests, os, time
 from discord.ext import commands
 import asyncio
@@ -50,7 +21,7 @@ init(autoreset=True)
 # 
 __TITLE__ = "C-REAL"
 __VERSION__ = "2.4.1"
-__AUTHOR__ = "TKperson"
+__AUTHOR__ = "mzk"
 __LICENSE__ = "MIT"
 
 # Global vars
@@ -83,9 +54,9 @@ def banner():
  ██████╗                  ██████╗ ███████╗ █████╗ ██╗     
 ██╔════╝                  ██╔══██╗██╔════╝██╔══██╗██║   Version: {__VERSION__}
 ██║         █████╗        ██████╔╝█████╗  ███████║██║     Made by:
-██║         ╚════╝        ██╔══██╗██╔══╝  ██╔══██║██║       TKperson
-╚██████╗                  ██║  ██║███████╗██║  ██║███████╗    and
- ╚═════╝                  ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝      cyxl
+██║         ╚════╝        ██╔══██╗██╔══╝  ██╔══██║██║       mzk
+╚██████╗                  ██║  ██║███████╗██║  ██║███████╗   
+ ╚═════╝                  ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝    
 '''.encode('utf8'))
 
 # Check for > 1.5.1 discord version
